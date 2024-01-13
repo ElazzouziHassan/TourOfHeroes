e8f3fcaaec9a338e2227b9f3ac117282be92c628
wizardy <wizardhassan99@gmail.com>
2024-01-13 20:59:02 +0100

    commit

--- src/app/component/heroes-list/heroes-list.component.ts
+++ src/app/component/heroes-list/heroes-list.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Hero } from '../../classes/hero';
+import { UpperCasePipe } from '@angular/common';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-heroes-list',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    UpperCasePipe,
   ],
   templateUrl: './heroes-list.component.html',
   styleUrl: './heroes-list.component.scss'
