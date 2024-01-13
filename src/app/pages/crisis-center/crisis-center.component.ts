88d27d4810d40947dd2c7a3d0ad5043b352c52cf
wizardy <wizardhassan99@gmail.com>
2024-01-13 19:54:54 +0100

    commit

--- src/app/pages/crisis-center/crisis-center.component.ts
+++ src/app/pages/crisis-center/crisis-center.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CrisisListComponent } from '../../component/crisis-list/crisis-list.component';
 
 @Component({
   selector: 'app-crisis-center',
   standalone: true,
-  imports: [],
+  imports: [
+    CrisisListComponent
+  ],
   templateUrl: './crisis-center.component.html',
   styleUrl: './crisis-center.component.scss'
 })
